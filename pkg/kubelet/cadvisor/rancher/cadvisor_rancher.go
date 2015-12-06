465058401b9bdd9e4e7aa392d2f07954274c7e27
ZhaoBin <antmanler@gmail.com>
2015-12-06 21:38:34 +0800

    Using an external cadviser when cloud provider is "Rancher"

---
+++ pkg/kubelet/cadvisor/rancher/cadvisor_rancher.go
@@ -0,0 +1,117 @@
+package rancher
+
+import (
+	"fmt"
+
+	"github.com/golang/glog"
+	"github.com/google/cadvisor/events"
+	cadvisorfs "github.com/google/cadvisor/fs"
+	cadvisorapi "github.com/google/cadvisor/info/v1"
+	cadvisorapiv2 "github.com/google/cadvisor/info/v2"
+	"k8s.io/kubernetes/pkg/kubelet/cadvisor"
+)
+
+type cadvisorClient struct {
+	*Client
+	em events.EventManager
+}
+
+var _ cadvisor.Interface = new(cadvisorClient)
+
+// Creates a cAdvisor.
+func New(url string) (cadvisor.Interface, error) {
+	// Create and start the cAdvisor client.
+	cli, err := NewClient(url)
+	if err != nil {
+		return nil, err
+	}
+
+	cadvisorClient := &cadvisorClient{
+		Client: cli,
+		em:     events.NewEventManager(events.DefaultStoragePolicy()),
+	}
+
+	return cadvisorClient, nil
+}
+
+func (cc *cadvisorClient) Start() error {
+	glog.V(2).Info("Using rancher provided cadvisor")
+	return nil
+}
+
+func (cc *cadvisorClient) VersionInfo() (*cadvisorapi.VersionInfo, error) {
+	attr, err := cc.Attributes()
+	if err != nil {
+		return nil, err
+	}
+	return &cadvisorapi.VersionInfo{
+		KernelVersion:      attr.KernelVersion,
+		ContainerOsVersion: attr.ContainerOsVersion,
+		DockerVersion:      attr.DockerVersion,
+		CadvisorVersion:    attr.CadvisorVersion,
+		CadvisorRevision:   "unknown",
+	}, nil
+}
+
+func (cc *cadvisorClient) SubcontainerInfo(name string, req *cadvisorapi.ContainerInfoRequest) (map[string]*cadvisorapi.ContainerInfo, error) {
+	infos, err := cc.Client.SubcontainersInfo(name, req)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make(map[string]*cadvisorapi.ContainerInfo, len(infos))
+	for _, info := range infos {
+		result[info.Name] = info
+	}
+	return result, nil
+}
+
+func (cc *cadvisorClient) DockerImagesFsInfo() (cadvisorapiv2.FsInfo, error) {
+	return cc.getFsInfo(cadvisorfs.LabelDockerImages)
+}
+
+func (cc *cadvisorClient) RootFsInfo() (cadvisorapiv2.FsInfo, error) {
+	return cc.getFsInfo(cadvisorfs.LabelSystemRoot)
+}
+
+func (cc *cadvisorClient) getFsInfo(label string) (cadvisorapiv2.FsInfo, error) {
+	res, err := cc.StorageInfo(label)
+	if err != nil {
+		return cadvisorapiv2.FsInfo{}, err
+	}
+	if len(res) == 0 {
+		return cadvisorapiv2.FsInfo{}, fmt.Errorf("failed to find information for the filesystem labeled %q", label)
+	}
+	// TODO(vmarmol): Handle this better when a label has more than one image filesystem.
+	if len(res) > 1 {
+		glog.Warningf("More than one filesystem labeled %q: %#v. Only using the first one", label, res)
+	}
+
+	return res[0], nil
+}
+
+func (cc *cadvisorClient) WatchEvents(request *events.Request) (*events.EventChannel, error) {
+	ec, err := cc.em.WatchEvents(request)
+	if err != nil {
+		return nil, err
+	}
+
+	// forward events
+	go func() {
+		evFw := make(chan *cadvisorapi.Event, 1)
+		// request from remote
+		err := cc.EventStreamingInfo(request, evFw)
+		if err != nil {
+			glog.Errorf("Can not get event stream from cadvisor, %e", err)
+			return
+		}
+
+		for e := range evFw {
+			cc.em.AddEvent(e)
+		}
+		// TODO(antmanler): Should we try to send request again?
+		glog.V(2).Infof("Cadvisor event forwarder for %q, ended", request.ContainerName)
+	}()
+
+	return ec, nil
+}
---
+++ pkg/kubelet/cadvisor/rancher/client.go
@@ -0,0 +1,276 @@
+package rancher
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/google/cadvisor/events"
+	info "github.com/google/cadvisor/info/v1"
+	infov2 "github.com/google/cadvisor/info/v2"
+)
+
+// Client represents the base URL for a cAdvisor client.
+type Client struct {
+	baseUrl string
+}
+
+// NewClient returns a new client with the specified base URL.
+func NewClient(url string) (*Client, error) {
+	if !strings.HasSuffix(url, "/") {
+		url += "/api/"
+	}
+
+	return &Client{
+		baseUrl: url,
+	}, nil
+}
+
+// Returns the JSON container information for the specified
+// Docker container and request.
+func (self *Client) DockerContainer(name string, query *info.ContainerInfoRequest) (cinfo info.ContainerInfo, err error) {
+	u := self.dockerInfoUrl(name)
+	ret := make(map[string]info.ContainerInfo)
+	if err = self.httpGetJsonData(&ret, query, u, fmt.Sprintf("Docker container info for %q", name)); err != nil {
+		return
+	}
+	if len(ret) != 1 {
+		err = fmt.Errorf("expected to only receive 1 Docker container: %+v", ret)
+		return
+	}
+	for _, cont := range ret {
+		cinfo = cont
+	}
+	return
+}
+
+// ContainerInfo returns the JSON container information for the specified
+// container and request.
+func (self *Client) ContainerInfo(name string, query *info.ContainerInfoRequest) (cinfo *info.ContainerInfo, err error) {
+	u := self.containerInfoUrl(name)
+	ret := new(info.ContainerInfo)
+	if err = self.httpGetJsonData(ret, query, u, fmt.Sprintf("container info for %q", name)); err != nil {
+		return
+	}
+	cinfo = ret
+	return
+}
+
+// Returns the information about all subcontainers (recursive) of the specified container (including itself).
+func (self *Client) SubcontainersInfo(name string, query *info.ContainerInfoRequest) ([]*info.ContainerInfo, error) {
+	var response []*info.ContainerInfo
+	url := self.subcontainersInfoUrl(name)
+	err := self.httpGetJsonData(&response, query, url, fmt.Sprintf("subcontainers container info for %q", name))
+	if err != nil {
+		return nil, err
+
+	}
+	return response, nil
+}
+
+// MachineInfo returns the JSON machine information for this client.
+// A non-nil error result indicates a problem with obtaining
+// the JSON machine information data.
+func (self *Client) MachineInfo() (minfo *info.MachineInfo, err error) {
+	u := self.machineInfoUrl()
+	ret := new(info.MachineInfo)
+	if err = self.httpGetJsonData(ret, nil, u, "machine info"); err != nil {
+		return
+	}
+	minfo = ret
+	return
+}
+
+// Attributes returns hardware and software attributes of the machine.
+func (self *Client) Attributes() (attr *infov2.Attributes, err error) {
+	u := self.attributesUrl()
+	ret := new(infov2.Attributes)
+	if err = self.httpGetJsonData(ret, nil, u, "attributes"); err != nil {
+		return
+	}
+	attr = ret
+	return
+}
+
+// Returns usage information about the filesystem .
+func (self *Client) StorageInfo(label string) (sinfo []infov2.FsInfo, err error) {
+	u := self.storageInfoUrl(label)
+
+	if err = self.httpGetJsonData(&sinfo, nil, u, "storage info"); err != nil {
+		return
+	}
+	return
+}
+
+// Streams all events that occur that satisfy the request into the channel
+// that is passed
+func (self *Client) EventStreamingInfo(request *events.Request, einfo chan *info.Event) (err error) {
+	u := self.eventsInfoUrl(request)
+	if err = self.getEventStreamingData(u, einfo); err != nil {
+		return
+	}
+	return nil
+}
+
+func (self *Client) dockerInfoUrl(name string) string {
+	return self.versionedUrl("v1.3", "docker", name)
+}
+
+func (self *Client) containerInfoUrl(name string) string {
+	return self.versionedUrl("v1.3", "containers", name)
+}
+
+func (self *Client) subcontainersInfoUrl(name string) string {
+	return self.versionedUrl("v1.3", "subcontainers", name)
+}
+
+func (self *Client) machineInfoUrl() string {
+	return self.versionedUrl("v1.3", "machine")
+}
+
+func (self *Client) attributesUrl() string {
+	return self.versionedUrl("v2.0", "attributes")
+}
+
+func (self *Client) storageInfoUrl(label string) string {
+	if label != "" {
+		q := url.Values{}
+		q.Add("label", label)
+		label = "?" + q.Encode()
+	}
+	return self.versionedUrl("v2.0", "storage") + label
+}
+
+func (self *Client) eventsInfoUrl(request *events.Request) string {
+	eventTypes := map[info.EventType]string{
+		info.EventOom:               "oom_events",
+		info.EventOomKill:           "oom_kill_events",
+		info.EventContainerCreation: "creation_events",
+		info.EventContainerDeletion: "deletion_events",
+	}
+
+	q := url.Values{}
+	q.Add("stream", "true")
+
+	for et, val := range request.EventType {
+		if ev, ok := eventTypes[et]; ok {
+			q.Add(ev, strconv.FormatBool(val))
+		}
+	}
+
+	if request.IncludeSubcontainers {
+		q.Add("subcontainers", "true")
+	}
+
+	if request.MaxEventsReturned > 0 {
+		q.Add("max_events", strconv.Itoa(request.MaxEventsReturned))
+	}
+
+	if !request.StartTime.IsZero() {
+		q.Add("start_time", request.StartTime.Format(time.RFC3339))
+	}
+	if !request.EndTime.IsZero() {
+		q.Add("end_time", request.EndTime.Format(time.RFC3339))
+	}
+
+	query := q.Encode()
+	if query != "" {
+		query = "?" + query
+	}
+
+	name := request.ContainerName
+	return self.versionedUrl("v1.3", "events", name) + query
+}
+
+func (self *Client) versionedUrl(version string, components ...string) string {
+	return self.baseUrl + version + "/" + path.Join(components...)
+}
+
+func (self *Client) httpGetResponse(postData interface{}, url, infoName string) ([]byte, error) {
+	var resp *http.Response
+	var err error
+
+	if postData != nil {
+		data, marshalErr := json.Marshal(postData)
+		if marshalErr != nil {
+			return nil, fmt.Errorf("Unable to marshal data: %v", marshalErr)
+		}
+		resp, err = http.Post(url, "application/json", bytes.NewBuffer(data))
+	} else {
+		resp, err = http.Get(url)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("Unable to post %q to %q: %v", infoName, url, err)
+	}
+	if resp == nil {
+		return nil, fmt.Errorf("Received empty response for %q from %q", infoName, url)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		err = fmt.Errorf("Unable to read all %q from %q: %v", infoName, url, err)
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("Request %q failed with error: %q", url, strings.TrimSpace(string(body)))
+	}
+	return body, nil
+}
+
+func (self *Client) httpGetString(url, infoName string) (string, error) {
+	body, err := self.httpGetResponse(nil, url, infoName)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (self *Client) httpGetJsonData(data, postData interface{}, url, infoName string) error {
+	body, err := self.httpGetResponse(postData, url, infoName)
+	if err != nil {
+		return err
+	}
+	if err = json.Unmarshal(body, data); err != nil {
+		err = fmt.Errorf("Unable to unmarshal %q (Body: %q) from %q with error: %v", infoName, string(body), url, err)
+		return err
+	}
+	return nil
+}
+
+func (self *Client) getEventStreamingData(url string, einfo chan *info.Event) error {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Status code is not OK: %v (%s)", resp.StatusCode, resp.Status)
+	}
+
+	dec := json.NewDecoder(resp.Body)
+	var m *info.Event = &info.Event{}
+	for {
+		err := dec.Decode(m)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			// if called without &stream=true will not be able to parse event and will trigger fatal
+			glog.Fatalf("Received error %v", err)
+		}
+		einfo <- m
+	}
+	return nil
+}
