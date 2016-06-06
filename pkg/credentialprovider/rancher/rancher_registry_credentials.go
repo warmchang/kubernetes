3bc0e28e48c4660dc18be757c325ab9d32460955
Alena Prokharchyk <alena@rancher.com>
2016-06-06 11:10:20 -0700

    Private registry test

--- pkg/credentialprovider/rancher/rancher_registry_credentials.go
+++ pkg/credentialprovider/rancher/rancher_registry_credentials.go
@@ -8,13 +8,23 @@ import (
 	"time"
 )
 
-// rancher provider fetching credentials from rancher server
+// rancher provider
 type rancherProvider struct {
+	credGetter credentialsGetter
+}
+
+// credentials getter from Rancher private registry
+type rancherCredentialsGetter struct {
 	client *client.RancherClient
 }
 
 type rConfig struct {
 	Global configGlobal
+}
+
+// An interface for testing purposes.
+type credentialsGetter interface {
+	getCredentials() []registryCredential
 }
 
 type configGlobal struct {
@@ -33,22 +43,27 @@ func init() {
 	if err != nil {
 		glog.Errorf("Failed to get rancher client: %v", err)
 	}
+
+	rancherGetter := &rancherCredentialsGetter{
+		client: client,
+	}
+
 	credentialprovider.RegisterCredentialProvider("rancher-registry-creds",
 		&credentialprovider.CachingDockerConfigProvider{
-			Provider: &rancherProvider{client},
+			Provider: &rancherProvider{rancherGetter},
 			Lifetime: 30 * time.Minute,
 		})
 }
 
-// Assuming its always enabled
+// Assuming it's always enabled
 func (p *rancherProvider) Enabled() bool {
-	return p.client != nil
+	return p.credGetter != nil
 }
 
 // Provide implements DockerConfigProvider.Provide, refreshing Rancher tokens on demand
 func (p *rancherProvider) Provide() credentialprovider.DockerConfig {
 	cfg := credentialprovider.DockerConfig{}
-	for _, cred := range p.getRancherCredentials() {
+	for _, cred := range p.credGetter.getCredentials() {
 		entry := credentialprovider.DockerConfigEntry{
 			Username: cred.credential.PublicValue,
 			Password: cred.credential.SecretValue,
@@ -60,16 +75,16 @@ func (p *rancherProvider) Provide() credentialprovider.DockerConfig {
 	return cfg
 }
 
-func (p *rancherProvider) getRancherCredentials() []registryCredential {
+func (g *rancherCredentialsGetter) getCredentials() []registryCredential {
 	var registryCreds []registryCredential
-	credColl, err := p.client.RegistryCredential.List(client.NewListOpts())
+	credColl, err := g.client.RegistryCredential.List(client.NewListOpts())
 	if err != nil {
 		glog.Errorf("Failed to pull registry credentials from rancher %v", err)
 		return registryCreds
 	}
 	for _, cred := range credColl.Data {
 		registry := &client.Registry{}
-		if err = p.client.GetLink(cred.Resource, "registry", registry); err != nil {
+		if err = g.client.GetLink(cred.Resource, "registry", registry); err != nil {
 			glog.Errorf("Failed to pull registry from rancher %v", err)
 			return registryCreds
 		}
@@ -86,6 +101,11 @@ func getRancherClient() (*client.RancherClient, error) {
 	url := os.Getenv("CATTLE_URL")
 	accessKey := os.Getenv("CATTLE_ACCESS_KEY")
 	secretKey := os.Getenv("CATTLE_SECRET_KEY")
+
+	if url == "" || accessKey == "" || secretKey == "" {
+		return nil, nil
+	}
+
 	conf := rConfig{
 		Global: configGlobal{
 			CattleURL:       url,
