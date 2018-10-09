f1984fa35dee2cd2a4c81403874158468456fc05
Darren Shepherd <darren@rancher.com>
2018-10-09 14:41:20 -0700

    Allow override of kubeconfig dialer

--- cmd/kube-apiserver/app/server.go
+++ cmd/kube-apiserver/app/server.go
@@ -94,6 +94,10 @@ const (
 	etcdRetryInterval = 1 * time.Second
 )
 
+var (
+	DefaultProxyDialerFn utilnet.DialFunc
+)
+
 // NewAPIServerCommand creates a *cobra.Command object with default parameters
 func NewAPIServerCommand(stopCh <-chan struct{}) *cobra.Command {
 	s := options.NewServerRunOptions()
@@ -169,6 +173,10 @@ func CreateServerChain(completedOptions completedServerRunOptions, stopCh <-chan
 	nodeTunneler, proxyTransport, err := CreateNodeDialer(completedOptions)
 	if err != nil {
 		return nil, err
+	}
+
+	if DefaultProxyDialerFn != nil {
+		completedOptions.KubeletConfig.Dial = DefaultProxyDialerFn
 	}
 
 	kubeAPIServerConfig, insecureServingInfo, serviceResolver, pluginInitializer, err := CreateKubeAPIServerConfig(completedOptions, nodeTunneler, proxyTransport)
