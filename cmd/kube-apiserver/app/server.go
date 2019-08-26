e7b9138aeaeaaa35ba0b89de025515409d0a1131
Darren Shepherd <darren@rancher.com>
2019-08-26 15:55:35 -0700

    Add back stopCh to cmd apis

--- cmd/kube-apiserver/app/server.go
+++ cmd/kube-apiserver/app/server.go
@@ -95,7 +95,7 @@ const (
 )
 
 // NewAPIServerCommand creates a *cobra.Command object with default parameters
-func NewAPIServerCommand() *cobra.Command {
+func NewAPIServerCommand(stopCh <-chan struct{}) *cobra.Command {
 	s := options.NewServerRunOptions()
 	cmd := &cobra.Command{
 		Use: "kube-apiserver",
@@ -118,7 +118,7 @@ cluster's shared state through which all other components interact.`,
 				return utilerrors.NewAggregate(errs)
 			}
 
-			return Run(completedOptions, genericapiserver.SetupSignalHandler())
+			return Run(completedOptions, stopCh)
 		},
 	}
 
